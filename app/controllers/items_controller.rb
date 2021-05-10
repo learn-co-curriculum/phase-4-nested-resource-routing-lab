ed42fd85de3c5c96c4ed4ab773bc14333e70f739
ihollander <ianholla@gmail.com>
2021-05-10 09:20:07 -0400

    Added solution

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      return render json: { error: "User not found" }, status: :not_found unless user
+      
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find_by(id: params[:id])
+    if item 
+      render json: item
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.create(item_params)
+      render json: item, status: :created
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
 end
