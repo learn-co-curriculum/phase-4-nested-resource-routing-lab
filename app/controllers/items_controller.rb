bf905737605af1b71c6015e5c93281cbf6c1145b
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-03 11:11:35 +0300

    commit tree 03acdcd8e23c33dbb55d8d8b35bfb7bd08917b2b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "User not found" }, status: :not_found
+  end
+
 
 end
