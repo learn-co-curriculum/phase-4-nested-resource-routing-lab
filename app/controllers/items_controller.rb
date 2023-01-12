225600e36b6d1b1fff9a6d638de8ae2b0cb013df
Morgan VanYperen <morgvanny@gmail.com>
2023-01-12 15:33:01 -0800

    commit tree 0990e5eb64d1ad9c4c39d12a0ff8d081baa13e67

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   def index
-    items = Item.all
+    user_id = params[:user_id]
+    items = user_id ? User.find(user_id).items : Item.all
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found(error)
+    render json: { error: "#{error.model} not found" }, status: :not_found
+  end
+
+  def record_invalid(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
